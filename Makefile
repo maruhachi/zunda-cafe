f61bab00c44007d2b39a9988a772e9a67bf8c6f2
ykonno <konchan1129@gmail.com>
2017-06-16 17:35:44 +0900

    makefile修正, themeを見やすく

--- Makefile
+++ Makefile
@@ -18,3 +18,9 @@ help:
 # "make mode" option.  $(O) is meant as a shortcut for $(SPHINXOPTS).
 %: Makefile
 	@$(SPHINXBUILD) -M $@ "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
+
+gh-pages:
+	rm -rf ./docs
+	make html
+	mv -fv build/html/* ./docs
+	touch ./docs/.nojekyll
--- source/conf.py
+++ source/conf.py
@@ -83,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -100,7 +100,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'zunda-cafedoc'
+htmlhelp_basename = 'zunda-cafe doc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
