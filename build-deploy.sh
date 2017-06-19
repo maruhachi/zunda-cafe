21ffd2f39f3cf1678e6d4b07f4aa39ec7ead772d
ykonno <konchan1129@gmail.com>
2017-06-19 14:41:23 +0900

    ignore gitignore.

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# TravisCI用のビルド・デプロイスクリプト
 set -e
 
 # sphinx ビルド. GitHubPages用ターゲット
@@ -7,6 +8,6 @@ make gh-pages
 # GitHub Pages 向けに /docsをコミット
 git config --global user.email "travis@example.com"
 git config --global user.name "travisci"
-git add docs/.
+git add docs/. -f
 git commit -m 'Generated automatically by TravisCI. deploy to /docs.'
 git push --quiet "https://${GH_TOKEN}@github.com/maruhachi/zunda-cafe.git" master:master > /dev/null 2>&1
