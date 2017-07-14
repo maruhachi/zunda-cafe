7620e7f539e3a04242d14464d8dd7730e47b6a55
ykonno <konchan1129@gmail.com>
2017-07-14 15:43:15 +0900

    no suppress message when push.
    no suppress message when push.

--- build-deploy.sh
+++ build-deploy.sh
@@ -10,4 +10,4 @@ git config --global user.email "travis@example.com"
 git config --global user.name "travisci"
 git add docs/. -f
 git commit -m 'Generated automatically by TravisCI. deploy to /docs.'
-git push --quiet "https://${GH_TOKEN}@github.com/maruhachi/zunda-cafe.git" master:master > /dev/null 2>&1
+git push "https://${GH_TOKEN}@github.com/maruhachi/zunda-cafe.git" master:master 
