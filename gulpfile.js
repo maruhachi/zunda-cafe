8b28c60032aa315bae0416f16c8b6070434ca373
ykonno <konchan1129@gmail.com>
2017-07-14 15:26:23 +0900

    gulpでwatchしてbuildしてlivereload したかった(未解決のエラー)

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
-var gulp = require('gulp')
-webserver = require('gulp-webserver');
+/** reSTファイルの変更を感知、ビルドしてブラウザでLiveReload */
+
+var gulp = require('gulp'),
+  webserver = require('gulp-webserver'),
+  shell = require('gulp-shell');
 
 var SERVER_PORT = 4000;
 
-gulp.task('default', ['serve']);
+
+gulp.task('watch', function () {
+  gulp.watch(['./source/*.rst', './source/**/*.rst'],
+      shell.task(['make html'])
+  );
+});
 
 gulp.task('serve', function () {
-  gulp.src('./docs')
+  gulp.src('./build/html')
     .pipe(webserver({
       livereload: true,
       open: true,
       port: SERVER_PORT
     }));
 });
+
+gulp.task('default', ['watch', 'serve']);
